12da6ca3dee69ecdec5793f4f322f09157d279ce
Seosamh Cahill <seo.cahill@gmail.com>
2016-11-15 17:34:33 +0100

    some debuggin info

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'nokogiri'
 require 'json'
+require 'pry'
 
 configure { set :server, :puma }
 
@@ -46,6 +47,9 @@ class XbrlInstanceValidator
     rescue Nokogiri::XML::SyntaxError => e
       errors << "caught exception: #{e}"
     end
+    puts "=" * 99
+    puts "well formed validation complete"
+    puts "=" * 99
     errors
   end
 
@@ -57,49 +61,72 @@ class XbrlInstanceValidator
       xsd = Nokogiri::XML::Schema(File.open(file))
       lines = ixbrl_instance.each_line
       xsd.validate(parsed_doc).each do |error|
+        binding.pry
         extract = lines.take(error.line).last(5) rescue "no extract"
         errors << {line: error.line, message: error.message, extract: extract}
       end
     rescue Nokogiri::XML::SyntaxError => e
       errors << "caught exception: #{e}"
     end
+    puts "=" * 99
+    puts "xhtml schema valid"
+    puts "=" * 99
     errors
   end
 
   def taxonomy_schema_ref_valid(params, errors)
+    puts "=" * 99
+    puts "schema ref valid"
+    puts "=" * 99
     errors
   end
 
   def ixbrl_spec_rules(params, errors)
+    puts "=" * 99
+    puts "ixbrl spec rules validation complete"
+    puts "=" * 99
     errors
   end
 
   def dts_discovery(params, errors)
+    puts "=" * 99
+    puts "dts dts_discovery complete"
+    puts "=" * 99
     errors
   end
 
   def xbrl_instance_schema_valid(params, errors)
-    xbrl_instance = params["xbrl_instance"]
-    begin
-      parsed_doc = Nokogiri::XML(xbrl_instance) { |config| config.strict }
-      xsd_entry_file = "xsd/ie/ie-gaap-main-2012-12-01.xsd"
-      xsd = Nokogiri::XML::Schema(File.open(xsd_entry_file))
-      lines = xbrl_instance.each_line
-      xsd.validate(parsed_doc).each do |error|
-        extract = lines.take(error.line).last(5) rescue "no extract"
-        errors << {line: error.line, message: error.message, extract: extract}
-      end
-    rescue Nokogiri::XML::SyntaxError => e
-      errors << "caught exception: #{e}"
-    end
+    # xbrl_instance = params["xbrl_instance"]
+    # begin
+    #   parsed_doc = Nokogiri::XML(xbrl_instance) { |config| config.strict }
+    #   xsd_entry_file = "xsd/ie/ie-gaap-main-2012-12-01.xsd"
+    #   xsd = Nokogiri::XML::Schema(File.open(xsd_entry_file))
+    #   lines = xbrl_instance.each_line
+    #   xsd.validate(parsed_doc).each do |error|
+    #     extract = lines.take(error.line).last(5) rescue "no extract"
+    #     errors << {line: error.line, message: error.message, extract: extract}
+    #   end
+    # rescue Nokogiri::XML::SyntaxError => e
+    #   errors << "caught exception: #{e}"
+    # end
+    #
+    puts "=" * 99
+    puts "xbrl schema valid"
+    puts "=" * 99
     errors
   end
 
   def xbrl_spec_rules(params, errors)
+    puts "=" * 99
+    puts "xbrl spec rules validation complete"
+    puts "=" * 99
     errors
   end
 
   def custom_rules(params, errors)
+    puts "=" * 99
+    puts "xbrl spec custom rules validation complete"
+    puts "=" * 99
     errors
   end
 end

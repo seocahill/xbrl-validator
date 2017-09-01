0f0f1a724f69b06b15d699c2cc16f75e7cd97f78
Seosamh Cahill <seo.cahill@gmail.com>
2017-09-01 17:24:14 +0200

    Test presence all tags

--- app.rb
+++ app.rb
@@ -22,6 +22,13 @@ end
 
 class BusinessRulesValidator
 
+  MANDATORY_TAGS = [
+    'uk-bus:EntityCurrentLegalOrRegisteredName',
+    'uk-bus:StartDateForPeriodCoveredByReport',
+    'uk-bus:EndDateForPeriodCoveredByReport',
+    'uk-gaap:ProfitLossOnOrdinaryActivitiesBeforeTax'
+  ]
+
   def initialize(params)
     @messages = {}
     file = File.open(File.join('/ixbrl', params["filename"]))
@@ -30,7 +37,7 @@ class BusinessRulesValidator
 
   def validate_document
     @messages[:schema_ref_test] = schema_ref_test
-    @messages[:company_name_present] = company_name_present
+    @messages[:mandatory_tags_present] = mandatory_tags_present
     @messages
   end
 
@@ -44,13 +51,48 @@ class BusinessRulesValidator
     end
   end
 
-  def company_name_present
-    company_name = @doc.xpath("//ix:nonNumeric[@name='uk-bus:EntityCurrentLegalOrRegisteredName']")&.first&.text
-    if !company_name.to_s.strip.empty?
-      "valid"
+  def mandatory_tags_present
+    missing_tags = check_for_missing_tags
+    if missing_tags.any?
+      "invalid: the following tags are missing or empty #{missing_tags.join('; ')}"
     else
-      "invalid: uk-bus:EntityCurrentLegalOrRegisteredName must be present"
+      "valid"
     end
+  end
+
+  def check_for_missing_tags
+    MANDATORY_TAGS.map do |tag|
+      tag_content = @doc.xpath("//*[@name='#{tag}']")&.first&.text
+      tag if tag_content.to_s.strip.empty?
+    end.compact
+  end
+  
+  def test_period_dates
+    skip 
+    # Report period start date cannot be later than the selected Revenue accounting period start date (<value>).
+    # Period End Date (uk- bus:EndDateForPeriodCoveredByRepo rt) is <end_date> but must be 2011-12- 31 or later
+    # Report period end date cannot be before the selected Revenue accounting period end date (<value>).
+    # Report period end date must fall within the selected Revenue accounting period.
+  end
+
+  def test_duplicate_facts
+    skip # Inconsistent duplicate facts, <fact name>, for context <context>.
+  end
+
+  def context_id_scheme
+    skip # Context entity identifier scheme (<value>) is not supported n/a
+  end
+
+  def context_schemes_consistent
+    skip # Contexts do not all use the same identifier and the same scheme.
+  end
+
+  def cro_number
+    skip # Companies Registration Office Number (ie- common:CompaniesRegistrationOffice Number) is missing If there is at least one context entity where the identifier scheme is 'http://www.cro.ie/'
+  end
+
+  def context_entity_id
+    skip # Context entity identifier (<value>) is not consistent with Revenue records (<value)>)
   end
 end
 

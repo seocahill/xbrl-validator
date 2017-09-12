e194861299ce71c92111c571ab19ed8b818ac791
Seosamh Cahill <seo.cahill@gmail.com>
2017-09-12 13:27:41 +0200

    Build xbrl file from scratch

--- lib/converter.rb
+++ lib/converter.rb
@@ -4,37 +4,65 @@ class Converter
 
   def initialize(ixbrl_doc)
     @ixbrl_doc = ixbrl_doc
-    xbrl_base_file = File.open('xbrl-base.xml')
-    @xbrl_doc = Nokogiri::XML(xbrl_base_file)
   end
 
   def to_xbrl
-    exclude_attributes = ["name", "scale", "format", "order", "tupleRef", "tupleID", "sign"]
+    # ixbrl attributes need to be stripped from facts
+    excluded_attributes = ["name", "scale", "format", "order", "tupleRef", "tupleID", "sign"]
+
+    # extract relevant sections
+    schema_ref = @ixbrl_doc.search('link|schemaRef')
+    units = @ixbrl_doc.search('xbrli|unit')
     facts = @ixbrl_doc.search("ix|nonNumeric", "ix|nonFraction")
     tuples = @ixbrl_doc.search("ix|tuple")
     contexts = @ixbrl_doc.search("xbrli|context")
 
-    context_insert_node = @xbrl_doc.search('xbrli|unit').last
+    # build base xbrl document from ixbrl file
+    namespaces = @ixbrl_doc.collect_namespaces
+    xbrl_base = Nokogiri::XML::Builder.new { |xml| xml['xbrli'].xbrl(namespaces) }
+    @xbrl_doc = Nokogiri.XML(xbrl_base.to_xml)
 
+    # Add schemaRef
+    schema_ref_node = schema_ref.first.clone
+    @xbrl_doc.root.add_child(schema_ref_node)
+
+    # keep track of the insert point
+    insert_node = schema_ref_node
+
+    # Add units
+    units.each do |unit|
+      unit_node = unit.clone
+      insert_node.add_next_sibling(unit_node)
+      insert_node = unit_node
+    end
+
+    # Add contexts
     contexts.each do |context|
-      copy_context = context.clone
-      context_insert_node.add_next_sibling(copy_context)
+      context_node = context.clone
+      insert_node.add_next_sibling(context_node)
+      insert_node = context_node
     end
 
-    fact_insert_node = @xbrl_doc.search('xbrli|context').last
+    # Add tuples
+    tuples.each do |tuple|
+      tuple_node = @xbrl_doc.create_element(tuple.attributes["name"].value, tuple.content)
+      tuple.attributes.each { |k,v| tuple_node[k] = v }
+      insert_node.add_next_sibling(tuple_node)
+      insert_node = tuple_node
+    end
 
+    # Add facts
     facts.each do |fact|
-      next if fact.name == "exclude"
-      if fact.attributes.has_key?("name")
-        content = formatted_content(fact)
-        node = @xbrl_doc.create_element(fact.attributes["name"], content)
-        fact.attributes
-          .reject {|k, v| exclude_attributes.include?(k) }
-          .each { |k,v| node[k] = v }
-        fact_insert_node.add_next_sibling(node)
-      end
+      content = formatted_content(fact)
+      fact_node = @xbrl_doc.create_element(fact.attributes["name"].value, content)
+      fact.attributes
+        .reject { |k,v| excluded_attributes.include?(k) }
+        .each { |k,v| fact_node[k] = v }
+      insert_node.add_next_sibling(fact_node)
+      insert_node = fact_node
     end
 
+    # Save file to disk for validation
     File.open('xbrl.xml', 'w') { |file| file.write(@xbrl_doc.to_xml) }
   end
 
@@ -42,7 +70,7 @@ class Converter
     content = fact.content # doesnt seem to be stripped 
     format = fact.attributes["format"].value rescue nil
 
-    # remove ix:excludes
+    # remove content of ix:excludes nodes
     if fact.children.any?
       fact.children.select { |f| f.name == "exclude" }.map(&:content).each do |substr|
         content = content.gsub(substr, '')
